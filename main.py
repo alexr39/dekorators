04af9f5a66223770d373b411cdc938b915da0277
alex <rai>
2019-02-27 09:42:45 +0700

    tests

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-
-def gettalk(type='shout'):
-    def whisper(word='da'):
-        return word.lower()+ '.....'
-    def shout(word = 'da'):
-        return word.upper() + '!'
-
-    if type == 'shout':
-        return shout
-    else:
-        return whisper
-
-talk = gettalk()
-print(gettalk('whisper')())
+def shout(word="да"):
+    return word.capitalize() + "!"
 
 
+scream = shout
+
+# def gettalk(type='shout'):
+#     def whisper(word='da'):
+#         return word.lower()+ '.....'
+#     def shout(word = 'da'):
+#         return word.upper() + '!'
+#
+#     if type == 'shout':
+#         return shout
+#     else:
+#         return whisper
+#
+#
+# talk = gettalk()
+# print(talk())
+# print(gettalk('whisper')())
+
+
+def doSometheingBefore(func):
+    print('do something before function')
+    print (func())
+doSometheingBefore(scream)
