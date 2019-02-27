9083c2fdd5bb695f70f342f75c813b4cb54f3ed5
alex <rai>
2019-02-27 10:31:23 +0700

    tests

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def shout(word="да"):
-    return word.capitalize() + "!"
+# def shout(word="да"):
+#     return word.capitalize() + "!"
+#
 
-
-scream = shout
+# scream = shout
 
 # def gettalk(type='shout'):
 #     def whisper(word='da'):
@@ -21,7 +21,26 @@ scream = shout
 # print(gettalk('whisper')())
 
 
-def doSometheingBefore(func):
-    print('do something before function')
-    print (func())
-doSometheingBefore(scream)
+# def dosomethingbefore(func):
+#     print('do something before function')
+#     print (func())
+
+
+# dosomethingbefore(scream)
+
+def my_new_dekorator(a_function_to_decorate):
+    def the_wrapper_arround_original_function():
+        print('This is code before original function running\n')
+        a_function_to_decorate()
+        print('This is code after original function\n')
+
+    return the_wrapper_arround_original_function()
+
+@my_new_dekorator
+def a_stand_alone_function():
+    print('Just a simple function, please dont change me!\n')
+
+
+# a_stand_alone_function()
+
+a_stand_alone_function
