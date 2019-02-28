8e6fcbf5c6f0061a4972aa12d54254307becb1b0
alex <rai>
2019-02-28 17:54:27 +0700

    continue decorators leaning. Understand how to push arguments to decorator and function

--- main.py
+++ main.py
@@ -82,44 +82,113 @@
 # l.sayyourage(-3)
 
 
-def a_decorator_passing_arbitary_arguments(function_to_decorate):
-    def a_wrapper_accepting_arbitary_arguments(*args, **kwargs):
-        print('Pushed anything to me?:')
-        print(args)
-        print(kwargs)
-        function_to_decorate(*args, **kwargs)
-    return a_wrapper_accepting_arbitary_arguments
+# def a_decorator_passing_arbitary_arguments(function_to_decorate):
+#     def a_wrapper_accepting_arbitary_arguments(*args, **kwargs):
+#         print('Pushed anything to me?:')
+#         print(args)
+#         print(kwargs)
+#         function_to_decorate(*args, **kwargs)
+#     return a_wrapper_accepting_arbitary_arguments
+#
+#
+# @a_decorator_passing_arbitary_arguments
+# def function_with_no_argument():
+#     print('No arguments here')
+#
+# function_with_no_argument()
+#
+# @a_decorator_passing_arbitary_arguments
+# def function_with_arguments(a,b,c):
+#     print(a, b, c)
+#
+# function_with_arguments(1,2,3)
+#
+#
+# @a_decorator_passing_arbitary_arguments
+# def function_with_named_arguments(a, b, c, platypus = 'Why not?'):
+#     print('lovely {0} {1} {2}? {3}'.format(a, b, c, platypus))
+#
+# function_with_named_arguments('Bill', 'Lucy', 'Jack', platypus='Of course!')
+#
+#
+# class Mary(object):
+#
+#
+#     def __init__(self):
+#         self.age = 31
+#
+#     @a_decorator_passing_arbitary_arguments
+#     def sayyourname(self, lie = -3):
+#         print('I\'m {} yo'.format(self.age + lie))
+#
+# m = Mary()
+# m.sayyourname()
 
 
-@a_decorator_passing_arbitary_arguments
-def function_with_no_argument():
-    print('No arguments here')
+# def my_decorator(func):
+#     print("Я обычная функция")
+#     def wrapper():
+#         print ("Я - функция, возвращаемая декоратором")
+#         func()
+#     return wrapper
+#
+#
+# def lazy_function():
+#     print("zzzzzzzz")
+#
+# lazy_function = my_decorator(lazy_function)
+#
+# lazy_function()
 
-function_with_no_argument()
+# def decorator_maker():
+#
+#     print('Я создаю декораторы! я буду вызван только РАЗ: ' +\
+#           'когда ты попросишь меня создать декоратор')
+#
+#     def my_decorator(func):
+#
+#         print('Я - декоратор! я буду вызван только один раз: в момент декорирования функции!')
+#
+#         def wrapped():
+#             print('\nСама обертка декорируемой функции.'
+#                   'Будет вызываться каждый раз когда будет использована декорируемая функция'
+#                   'Я возвращаю результат работы декорируемой функции')
+#             return func()
+#
+#         print('Я возвращаю обернутую функцию')
+#         return wrapped
+#
+#     print('я возвращаю декоратор')
+#     return my_decorator
+#
+# @decorator_maker()
+# def decorated_function():
+#     print('\nЯ декорируемая функция\n')
+#
+#
+# decorated_function()
 
-@a_decorator_passing_arbitary_arguments
-def function_with_arguments(a,b,c):
-    print(a, b, c)
 
-function_with_arguments(1,2,3)
+def decorator_maker_with_args(decorator_arg1, decorator_arg2):
 
-
-@a_decorator_passing_arbitary_arguments
-def function_with_named_arguments(a, b, c, platypus = 'Why not?'):
-    print('lovely {0} {1} {2}? {3}'.format(a, b, c, platypus))
-
-function_with_named_arguments('Bill', 'Lucy', 'Jack', platypus='Of course!')
+    print('I\'m creating docorator! And I recieved following arguments:', decorator_arg1, decorator_arg2)
 
 
-class Mary(object):
+    def my_decorator(func):
+        print('\nThis is decorator and here are arguments',  decorator_arg1, decorator_arg2)
+
+        def wrapper(function_arg1, function_arg2):
+            print('I\'m wrapper around original function.\n'
+                  'And i have access to all arguments: \n'
+                  '\t - decorator arguments: {0} {1} \n'
+                  '\t - function arguments: {2} {3}.\n'
+                  'Now I can push arguments on'.format(decorator_arg1, decorator_arg2, function_arg1, function_arg2))
+            return func(function_arg1, function_arg2)
+        return wrapper
+    return my_decorator
+@decorator_maker_with_args('Leonard', 'Sheldon')
+def decorate_function_with_arguments(function_arg1, function_arg2):
+    print('I\'m function for decoration and I know only about my arguments {0} and {1}'.format(function_arg1, function_arg2))
 
 
-    def __init__(self):
-        self.age = 31
-
-    @a_decorator_passing_arbitary_arguments
-    def sayyourname(self, lie = -3):
-        print('I\'m {} yo'.format(self.age + lie))
-
-m = Mary()
-m.sayyourname()
+decorate_function_with_arguments('Radjesh', 'Hovard')
